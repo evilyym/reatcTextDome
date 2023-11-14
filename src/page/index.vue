9a578f914584d7ab4de9536dc13ced8cbb36ff6b
yym <yangym@golive.cc>
2023-11-14 11:43:41 +0800

    chore:正式服地址改变

--- src/page/index.vue
+++ src/page/index.vue
@@ -126,7 +126,7 @@ const onSubmit = () => {
   } else if(hostname.indexOf('localhost') > -1|| hostname.indexOf('127.0.0.1') > -1){
     bastUrl="/vvr_wap/api"
   }else{
-    bastUrl="https://doorctlapi.zjnu.edu.cn/vvr_api/"
+    bastUrl="https://zjnu-vvr.goliveplus.cn/vvr_api/"
   }
   axios
     // .post("api/visit/create_visit_record/", query, {
