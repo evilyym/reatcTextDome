520f67cceeba1752db5d90883bf0453a093f4529
yym <yangym@golive.cc>
2023-11-28 15:23:08 +0800

    chore 判断saaas路径

--- src/request/index.ts
+++ src/request/index.ts
@@ -7,22 +7,7 @@ declare module "axios" {
   }
   export function create(config?: AxiosRequestConfig): AxiosInstance;
 }
-import Vconsole from 'vconsole'
-
-let hostname = location.hostname;
-let configURL = { baseURL: "", saasURL: "" };
-if (
-  hostname.indexOf("dev") > -1 ||
-  hostname.indexOf("127.0.0.1") > -1 ||
-  hostname.indexOf("localhost") > -1
-) {
-  configURL.baseURL = "https://dev-zjtie.goliveplus.cn/activitysupport_api"; //测试服
-  configURL.saasURL = "https://dev-zjtie.goliveplus.cn"; //测试服
-  new Vconsole()
-} else {
-  configURL.baseURL = "https://zjtie.goliveplus.cn/activitysupport_api"; //正式服
-  configURL.saasURL = "https://zjtie.goliveplus.cn"; //正式服
-}
+import configURL from "./http";
 
 const request = axios.create({
   baseURL: configURL.baseURL, //'https://dev-zjtie.goliveplus.cn/activitysupport_api'
--- src/route/index.ts
+++ src/route/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { getInfoApi } from "../api/index.ts";
-
+import configURL from "../request/http";
 const routes = [
   {
     path: "/",
@@ -48,11 +48,11 @@ router.beforeEach(async (to: any, from, next) => {
     // https://dev-zjtie.goliveplus.cn/saas_wap/analysis?redirect_url=http://127.0.0.1:5173/activitysupport_wap/?code=0JALIIiG
 
     const point_url =
-      to.query.point_url || "https://dev-zjtie.goliveplus.cn/saas_wap/";
+      to.query.point_url || configURL.saasURL;
     location.replace(
       `${point_url}analysis?redirect_url=${
         location.origin + location.pathname
-      }?activitysupporCode=${to.query.code}&actType=22222`
+      }?activitysupporCode=${to.query.code}`
     );
   }
 
@@ -61,11 +61,11 @@ router.beforeEach(async (to: any, from, next) => {
     // redirect_url=http://127.0.0.1:5173/activitysupport_wap/eventDetails?id=3user_type=1&
     // point_url=https://dev-zjtie.goliveplus.cn/saas_wap/
     const point_url =
-      to.query.point_url || "https://dev-zjtie.goliveplus.cn/saas_wap/";
+      to.query.point_url || configURL.saasURL;
     location.replace(
       `${point_url}analysis?redirect_url=${
         location.origin + location.pathname
-      }?id=${to.query.id}&userType=${to.query.user_type}&aaaCode=1111`
+      }?id=${to.query.id}&userType=${to.query.user_type}`
     );
   }
   if (to.query.tenantCode) {
