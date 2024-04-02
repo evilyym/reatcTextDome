18c8063d1b9915b199f8bb9a5e7b53cbaa074d8a
yym <yangym@golive.cc>
2024-04-02 16:47:48 +0800

    研究动态路由 后端维护表 前端创建文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,25 @@
 /*
  * @Author: yym
  * @Date: 2024-01-26 01:29:24
- * @LastEditTime: 2024-03-18 10:52:52
+ * @LastEditTime: 2024-04-02 16:46:12
  */
 import { createBrowserRouter } from 'react-router-dom';
 
 import routes from './routes';
 
-//可传第二个参数，配置base路径 { basename: "/app"}
 const router = createBrowserRouter(routes);
+
+const modules = import.meta.glob('./pages/*/index.tsx');
+const components = Object.keys(modules).reduce<Record<string, any>>((prev, cur) => {
+  prev[cur.replace('./pages', '')] = modules[cur];
+  return prev;
+}, {}) as any;
+
+// 获取菜单后动态添加路由
+router.routes[0].children = adminMenus.map((menu: any) => ({
+  path: menu.route,
+  Component: lazy(components[menu.filePath]),
+}));
+//可传第二个参数，配置base路径 { basename: "/app"}
 
 export default router;
