49c57e78e2cc69cea660142298f6c5fbeb46f9b0
evily <evilyym@163.com>
2024-05-20 08:21:21 +0800

    8.20

--- src/pages/TypeScriptDome.ts
+++ src/pages/TypeScriptDome.ts
@@ -3,12 +3,43 @@ type stringType = string;
 interface IUser {
   name: string;
 }
-type stringType1 = stringType & number;
+type stringType1 = stringType | number;
 
 interface IUser1 extends IUser {
   age: null;
 }
 
-interface numberType {
-  num: number;
+type stringType2 = stringType1 | IUser1;
+
+type a = never; //不存在的值
+
+type stringAndNumber = string | number;
+
+const str: stringAndNumber = '123';
+
+interface Name {
+  name: string;
 }
+type User = Name & {
+  age: number;
+};
+
+const user: User = {
+  age: 22,
+  name: '11',
+};
+
+type typeBox = {
+  // 基础类型
+  [key: string]: null | undefined | void | string | number | boolean | symbol;
+};
+
+function fnType<T>(params: T): T {
+  const a: T = params;
+  return a;
+}
+
+const conFnType = <T>(data: string): T => {
+  const obj = { data } as T;
+  return obj;
+};
