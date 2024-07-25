ba7ee48946af1ff5bb40d7e7ac1817ab9f6af4ee
yym <yangym@golive.cc>
2024-07-25 16:18:14 +0800

    chore

--- look.js
+++ look.js
@@ -313,3 +313,37 @@ var summaryRanges = function (nums = [0, 2, 3, 4, 6, 8, 9]) {
 };
 
 // summaryRanges();
+
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge1 = function (
+  intervals = [
+    [1, 3],
+    [2, 6],
+    [8, 10],
+    [15, 18],
+  ],
+) {
+  let res = [];
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  let prev = intervals[0];
+
+  for (let i = 1; i < intervals.length; i++) {
+    let cur = intervals[i];
+    if (prev[1] >= cur[0]) {
+      prev[1] = Math.max(cur[1], prev[1]);
+    } else {
+      // 不重合，prev推入res数组
+      res.push(prev);
+      prev = cur; // 更新 prev
+    }
+  }
+
+  res.push(prev);
+  return res;
+};
+
+// merge1();
