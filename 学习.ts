c92b4c224ebdcdb350ea4fb98d814acb2aece8e0
yym <yangym@golive.cc>
2024-10-11 18:06:43 +0800

    1

--- 学习.ts
+++ 学习.ts
@@ -32,3 +32,27 @@ const f1 = <T>(data: T): T => {
 function identity<T>(arg: T): T {
   return arg;
 }
+interface Date {
+  format(fmt: string): string;
+}
+//为Date原型添加方法(js日期格式化)
+Date.prototype.format = function (fmt: string): string {
+  const o = {
+    'M+': this.getMonth() + 1, //月份
+    'd+': this.getDate(), //日
+    'h+': this.getHours(), //小时
+    'm+': this.getMinutes(), //分
+    's+': this.getSeconds(), //秒
+    'q+': Math.floor((this.getMonth() + 3) / 3), //季度
+    S: this.getMilliseconds(), //毫秒
+  };
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+  }
+  for (const k in o) {
+    if (new RegExp('(' + k + ')').test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ('00' + o[k]).substr(('' + o[k]).length));
+    }
+  }
+  return fmt;
+};
